f8587eeacb6095a23610ab49421415220c149482
DShivansh <dHoneysh@gmail.com>
2025-03-11 23:49:16 +0530

    Added parsing for boolean

--- src/learn_interpreter/evaluator.clj
+++ src/learn_interpreter/evaluator.clj
@@ -4,12 +4,30 @@
              ExpressionStatement IntegerLiteral PrefixExpression InfixExpression BooleanExpression IfExpression BlockStatement FunctionLiteral CallExpression]
    [learn_interpreter.object IntegerObj BooleanObj NullObj]))
 
-(defn eval [ast-node]
+(defn my-eval [ast-node]
+  (println)
+  (println " ast-node is " ast-node)
   (let [node ast-node ast-node-type (type node)]
+    (println " type is " ast-node-type)
+    (println (= (str learn_interpreter.ast.ExpressionStatement) (str ast-node-type)) " evaluation")
     (cond
-      (= learn_interpreter.ast.ExpressionStatement ast-node-type) (eval (.Expression node))
-      (= learn_interpreter.ast.IntegerLiteral ast-node-type) (IntegerObj. (.Value node))
-      :else nil)))
+      (= (str learn_interpreter.ast.ExpressionStatement) (str ast-node-type)) (do
+                                                                    (println)
+                                                                    (println " got inside expression block")
+                                                                    (my-eval (.Expression node)))
+      (= (str learn_interpreter.ast.IntegerLiteral) (str ast-node-type)) (do
+                                                               (println)
+                                                               (println " got inside IntegerLiteral block")
+                                                               (IntegerObj. (.Value node)))
+      (= (str learn_interpreter.ast.BooleanExpression) (str ast-node-type)) (do
+                                                                  (println)
+                                                                  (println " got inside BooleanExpression block")
+                                                                  (println "node is " node)
+                                                                  (println " value is " (.Value node))
+                                                                  (BooleanObj. (.Value node)))
+      :else (do
+              (println "inside the else block")
+              nil))))
 
 (defn eval-statements
   "This function will receive the list of parsed Statements
@@ -19,9 +37,15 @@
   (loop [[first-statement & rest-statements] statements result nil]
     (cond
       (nil? first-statement) result
-      :else (recur rest-statements (eval first-statement)))))
+      :else (recur rest-statements (my-eval first-statement)))))
 
 
-(eval-statements (parser/start "5;"))
-(parser/start "5;")
+;; (eval-statements (parser/start "5;"))
+;; (parser/start "5;")
+;; (def a (parser/start "true;"))
+;; (eval-statements a)
+;; (eval-statements (parser/start "true;"))
+;; (parser/start "true;")
+;; (str learn_interpreter.ast.ExpressionStatement)
+
 
